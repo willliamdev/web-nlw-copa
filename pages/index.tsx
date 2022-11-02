65699084dff39c0127467055de923779e20b0824
William Almeida <willliam.dev@gmail.com>
2022-11-02 01:00:48 -0300

    :fire: remove trash code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,6 @@
 
 export default function Home() {
   return (
-    <>
-      <h1>Hello World</h1>
-      <code> npm i prisma-erd-generator @mermaid-js/mermaid-cli -D</code>
-    </>
+    <h1>Hello World</h1>
   )
 }
